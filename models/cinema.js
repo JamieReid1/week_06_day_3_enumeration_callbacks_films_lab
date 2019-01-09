84ec59dc924859f6baca8c18be361b329f5a48ed
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-09 14:25:24 +0000

    Setup start code files, add Cinema filmTitles function with passing test.

---
+++ models/cinema.js
@@ -0,0 +1,12 @@
+const Cinema = function (films) {
+  this.films = films;
+
+
+Cinema.prototype.filmTitles = function () {
+  return this.films.map(film => film.title);
+};
+
+
+};
+
+module.exports = Cinema;
